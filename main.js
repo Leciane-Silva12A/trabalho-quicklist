fe52da6ab1f7e753ea8e48c1e76ddaa82530eafd
Leciane Leonardo da Silva <leciane.leonardo@eaportal.org>
2025-06-08 18:00:24 +0000

    último ajuste da função checkItem

--- main.js
+++ main.js
@@ -59,11 +59,7 @@ const divWarning = document.querySelector(".warning")
 function checkItem(itemName) {
     const item = items.find((item) => item.name === itemName)
 
-    if(item.checked === true) {
-        item.checked = false
-    } else {
-        item.checked = true
-    }
+    item.checked = !item.checked
 
     showItemsList()
 }
